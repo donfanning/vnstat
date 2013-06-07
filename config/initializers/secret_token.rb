74ce9c37edf6c0de30cb17c92bd18af3281ee7fc
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-06-06 22:30:31 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vnstat::Application.config.secret_token = '0ebefc544df4463445784ed033698fdb4de54c57b522796b8de5dd6599cbc04bc25ba0cbca0695255108f1d8062ac1ff025d0faa1c708605b6bd4a037a057cf9'
