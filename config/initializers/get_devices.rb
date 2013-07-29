e1195f9635efbba63d356226ec199cfb06d60943
Marvin Frederickson <marvin.frederickson@gmail.com>
2013-07-29 11:24:29 -0800

    handle multiple devices

--- config/initializers/get_devices.rb
+++ config/initializers/get_devices.rb
@@ -1,16 +1,27 @@
+
+require "vnstats"
+include Vnstats
+
 # get tracked vnstat devices
 
 AVAILABLE_DEVICES = {}
 
-_if = ""
-_alias = ""
-`vnstat --dumpdb`.lines.each do |s|
-	if s.start_with? "interface;"
-		_if = s.split(";")[1].strip
-	end
-	if s.start_with? "nick;"
-		_alias = s.split(";")[1].strip
-		AVAILABLE_DEVICES[ _if.to_sym ] = _alias 
+`ls /var/lib/vnstat`.split.each do |d|
+	if d.start_with?("eth")
+		vnstat = fetch_vnstats(d)
+		if !vnstat.nil? and vnstat.has_key?(:device) and vnstat.has_key?(:nick)
+			AVAILABLE_DEVICES[ vnstat[:device].to_sym ] = vnstat[:nick]
+		end
 	end
 end
+
+# `vnstat --dumpdb`.lines.each do |s|
+# 	if s.start_with? "interface;"
+# 		_if = s.split(";")[1].strip
+# 	end
+# 	if s.start_with? "nick;"
+# 		_alias = s.split(";")[1].strip
+# 		AVAILABLE_DEVICES[ _if.to_sym ] = _alias 
+# 	end
+# end
 
