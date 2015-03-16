95febc31193968f9de999e0bce91fbfb6f388ff8
Marvin Frederickson <marvin.frederickson@gmail.com>
2015-03-16 10:06:19 -0800

    upgrade to rails 4.2

--- config/initializers/app_configuration.rb
+++ config/initializers/app_configuration.rb
@@ -1,12 +1,12 @@
-require 'yaml'
+# require 'yaml'
 
-# If no application configuration exists, copy the default one over...
-unless FileTest.exists?("config/application.yml")
-  FileUtils.cp "config/application.yml.sample", "config/application.yml"
-end
+# # If no application configuration exists, copy the default one over...
+# unless FileTest.exists?("config/application.yml")
+#   FileUtils.cp "config/application.yml.sample", "config/application.yml"
+# end
 
-#ENV.update YAML.load(File.read("config/application.yml"))
-APP_CONFIG = HashWithIndifferentAccess.new(YAML.load(File.read("config/application.yml")))
-#yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
-#APP_CONFIG = HashWithIndifferentAccess.new(yaml_data)
+# #ENV.update YAML.load(File.read("config/application.yml"))
+# APP_CONFIG = HashWithIndifferentAccess.new(YAML.load(File.read("config/application.yml")))
+# #yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
+# #APP_CONFIG = HashWithIndifferentAccess.new(yaml_data)
 
