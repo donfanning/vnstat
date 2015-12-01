44526c46dbafd372cde5833333ae3201a685f171
Marvin Frederickson <marvin.frederickson@gmail.com>
2015-12-01 10:50:04 -0900

    include devices other than eth*

--- config/initializers/get_devices.rb
+++ config/initializers/get_devices.rb
@@ -7,10 +7,10 @@ include Vnstats
 AVAILABLE_DEVICES = {}
 
 `ls /var/lib/vnstat`.split.each do |d|
-	if d.start_with?("eth")
+	#if d.start_with?("eth")
 		vnstat = fetch_vnstats(d.strip)
 		if !vnstat.nil? and vnstat.has_key?(:device) and vnstat.has_key?(:nick)
 			AVAILABLE_DEVICES[ vnstat[:device].to_sym ] = vnstat[:nick]
 		end
-	end
+	#end
 end
